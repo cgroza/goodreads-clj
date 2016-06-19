58105737d4869ef85e4f94d63a760739b482536e
Groza Cristian <kristi9524@gmail.com>
2016-06-18 21:27:18 -0400

    More functions.

--- src/goodreads_clj/goodreads.clj
+++ src/goodreads_clj/goodreads.clj
@@ -21,14 +21,22 @@
 (def url-find-group "/group/search.xml")
 (def url-info-group-id "/group/show/")
 (def url-listopia-book-id "/list/book/")
+(def url-user-read-status-id "/read_statuses/")
+(def url-recent-member-reviews "/review/recent_reviews.xml")
+(def url-review "/review/show.xml")
+(def url-user-review-book "/review/show_by_user_and_book.xml")
+(def url-find-book "/search/index.xml")
+(def url-series "/series/show/")
 ;; Default parameters
 (def default-data {"key" dev-key})
 
 (defn parametric [data]
   (string/join "&" (map (fn [[k v]] (str k "=" v)) data)))
 
-(defn url-get [api-url data]
-  (http/get (str root-url api-url "?" (parametric (conj default-data data)))))
+(defn url-get
+  ([api-url] (url-get api-url {}))
+  ([api-url data]
+   (http/get (str root-url api-url "?" (parametric (conj default-data data))))))
 
 (defn parse-xml-body [response]
   (->> response :body .getBytes java.io.ByteArrayInputStream. xml/parse))
@@ -50,8 +58,11 @@
 (defn info-author-id [id]
   (parse-xml-body (url-get url-info-author-id {"id" id})))
 
-(defn author-books-page [id page]
-  (parse-xml-body (url-get url-author-books-page {"id" id "page" (str page)})))
+(defn author-books-page
+  ([id] (author-books-page id 1))
+  ([id page]
+   (parse-xml-body (url-get url-author-books-page {"id" id "page" (str page)}))))
+
 
 (defn reviews-widget-title [title author & rating]
   (->> (let [data {"title" title "author" (encode-name author)}]
@@ -86,4 +97,37 @@
 
 (defn listopia-book-id [id]
   (->> (url-get (str url-listopia-book-id id ".xml"))
+       parse-xml-body))
+
+(defn user-read-status [id]
+  (->>
+   (url-get (str url-user-read-status-id id) {"format" "xml"})
+   (parse-xml-body)))
+
+(defn recent-member-reviews []
+  (parse-xml-body (url-get url-recent-member-reviews)))
+
+(defn review
+  ([id] (review id 1))
+  ([id page] (->> (url-get url-review {"id" id "page" (str page)})
+                  parse-xml-body)))
+
+(defn user-review-book
+  ([user-id book-id] (user-review-book user-id book-id nil))
+  ([user-id book-id include-review-on-work]
+   (->> {"user_id" user-id "book_id" book-id
+         "include_review_on_work" (if include-review-on-work "true" "false")}
+        (url-get url-user-review-book)
+        parse-xml-body)))
+
+(defn find-book
+  ([query-string] (find-book query-string 1))
+  ([query-string page] (find-book query-string page "all"))
+  ([query-string page field]
+   (->> {"q" (encode-name query-string)"page" (str page) "field" field}
+        (url-get url-find-book)
+        (parse-xml-body))))
+
+(defn series [id]
+  (->> (url-get (str url-series id) {"format" "xml"})
        parse-xml-body))
